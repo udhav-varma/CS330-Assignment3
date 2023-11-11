ec3f49ef02337f5784ddce6d70ee6fb8402a7a76
Udhav Varma <udhavv21@iitk.ac.in>
2023-11-11 17:36:14 +0000

    Part 2 done
    More testing needed

--- gemOS/src/user/init.c
+++ gemOS/src/user/init.c
@@ -2,33 +2,37 @@
 
 int main(u64 arg1, u64 arg2, u64 arg3, u64 arg4, u64 arg5)
 {
-
   int pages = 4096;
-
-  char * mm1 = mmap(NULL, pages*6, PROT_READ|PROT_WRITE, 0);
+  char * mm1 = mmap(NULL, pages*6, PROT_READ, 0);
   if((long)mm1 < 0)
   {
     // Testcase failed.
      printf("Test case failed \n");
     return 1;
   }
-  // vm_area count should be 1.
-  pmap(1);
-
-  unsigned long readonly_address = (unsigned long)mm1 + pages* 3;
-
-  // Should change access rights of the third page, existing vm_Area should be splitted up. A new vm area with access rights with PROT_READ will be created.
-  int result  = mprotect((void *)readonly_address, pages, PROT_READ);
-
+  //Read all the pages
+  for(int i = 0; i < 6; i++){
+        char temp;
+        char* page_read = mm1 +(i*pages);
+        temp = page_read[0];
+  }
+  //Page faults should be 6
+  pmap(0);
+  //changing the protection should give access to write
+  int result  = mprotect((void *)mm1, pages*6, PROT_READ|PROT_WRITE);
   if(result <0)
   {
-    // Testcase failed
      printf("Test case failed \n");
     return 0;
   }
-  
-  // vm_area count should be 3.
-  pmap(1);
+
+  for(int i = 0; i < 6; i++){
+        char* page_write = mm1 +(i*pages);
+        page_write[0] = 'A';
+  }
+  //Page faults should be 6
+  pmap(0);
 
   return 0;
 }
+
--- gemOS/src/v2p.c
+++ gemOS/src/v2p.c
@@ -9,6 +9,72 @@
  * You must not declare and use any static/global variables 
  * */
 
+int present_page(u64 page_entry)
+{
+    return (page_entry&1);
+}
+
+/**
+ * Dellocates page at `addr`
+*/
+void deallocate_page(struct exec_context * current, u64 addr)
+{
+    long pfn = current->pgd;
+    long * table, offset;
+    for(int c = 0; c < 4; c++){
+        table = osmap(pfn);
+        offset = ((addr >> (39 - 9*c)) & ((1ull << 9) - 1));
+        if(!present_page(table[offset])) return;
+        pfn = (table[offset] >> 12);
+    }
+
+    if(present_page(table[offset])){
+        os_pfn_free(USER_REG, pfn);
+        table[offset] ^= 1;
+    }
+    asm volatile("invlpg (%0)" :: "r" (addr) : "memory");
+}
+
+
+/**
+ * Changes permission of page
+*/
+void change_access(struct exec_context * current, u64 addr, int prot)
+{
+    long pfn = current->pgd;
+    long * table, offset;
+    for(int c = 0; c < 4; c++){
+        table = osmap(pfn);
+        offset = ((addr >> (39 - 9*c)) & ((1ull << 9) - 1));
+        if(!present_page(table[offset])) return;
+        pfn = (table[offset] >> 12);
+    }
+
+    if(prot&O_WRITE){
+        table[offset] |= (1ull << 3);
+    }
+    else{
+        if(table[offset]&(1ull << 3)) table[offset] ^= (1ull << 3);
+    }
+    asm volatile("invlpg (%0)" :: "r" (addr) : "memory");
+}
+
+
+void change_range_access(u64 start, u64 end, int prot, struct exec_context * curr)
+{
+    while(start < end){
+        change_access(curr, start, prot);
+        start += 4096;
+    }
+}
+
+void deallocate_range(u64 start, u64 end, struct exec_context * curr)
+{
+    while(start < end){
+        deallocate_page(curr, start);
+        start += 4096;
+    }
+}
 
 /**
  * mprotect System call Implementation.
@@ -25,6 +91,7 @@ long vm_area_mprotect(struct exec_context *current, u64 addr, int length, int pr
             if(addr == ptr->vm_start){
                 if(end >= ptr->vm_end){
                     ptr->access_flags = prot;
+                    change_range_access(addr, ptr->vm_end, prot, current);
                     addr = ptr->vm_end;
                 }
                 else{
@@ -36,6 +103,7 @@ long vm_area_mprotect(struct exec_context *current, u64 addr, int length, int pr
                     ptr->vm_next = newnode;
                     ptr->vm_end = end;
                     ptr->access_flags = prot;
+                    change_range_access(addr, end, prot, current);
                     addr = end;
                 }
             }
@@ -48,6 +116,7 @@ long vm_area_mprotect(struct exec_context *current, u64 addr, int length, int pr
                     newnode->access_flags = prot;
                     ptr->vm_end = addr;
                     ptr->vm_next = newnode;
+                    change_range_access(addr, ptr->vm_end, prot, current);
                     addr = ptr->vm_end;
                 }
                 else{
@@ -63,6 +132,7 @@ long vm_area_mprotect(struct exec_context *current, u64 addr, int length, int pr
                     rnode->vm_start = end;
                     ptr->vm_end = addr;
                     ptr->vm_next = bwnode;
+                    change_range_access(addr, end, prot, current);
                     addr = end;
                 }
             }
@@ -252,6 +322,7 @@ long vm_area_unmap(struct exec_context *current, u64 addr, int length)
         if(addr < ptr->vm_end){
             if(addr == ptr->vm_start){
                 if(end >= ptr->vm_end){
+                    deallocate_range(addr, ptr->vm_end, current);
                     addr = ptr->vm_end;
                     pptr->vm_next = ptr->vm_next;
                     struct vm_area * hold = ptr;
@@ -260,6 +331,7 @@ long vm_area_unmap(struct exec_context *current, u64 addr, int length)
                     continue;
                 }
                 else{
+                    deallocate_range(addr, end, current);
                     ptr->vm_start = end;
                     addr = end;
                 }
@@ -268,11 +340,13 @@ long vm_area_unmap(struct exec_context *current, u64 addr, int length)
                 if(end >= ptr->vm_end){
                     u64 hold = ptr->vm_end;
                     ptr->vm_end = addr;
+                    deallocate_range(addr, hold, current);
                     addr = hold;
                 }
                 else{
                     u64 hold = ptr->vm_end;
                     ptr->vm_end = addr;
+                    deallocate_range(addr, end, current);
                     addr = end;
                     struct vm_area * endnode = (struct vm_area *) os_alloc(sizeof(struct vm_area));
                     endnode->vm_start = end;
@@ -307,6 +381,15 @@ long vm_area_unmap(struct exec_context *current, u64 addr, int length)
 long vm_area_pagefault(struct exec_context *current, u64 addr, int error_code)
 {
     struct vm_area * ptr = current->vm_area;
+    if(ptr == NULL){
+        struct vm_area * headnode = (struct vm_area *) os_alloc(sizeof(struct vm_area));
+        headnode->access_flags = 0x0;
+        headnode->vm_start = MMAP_AREA_START;
+        headnode->vm_end = MMAP_AREA_START + 4096;
+        headnode->vm_next = NULL;
+        current->vm_area = headnode;
+        stats->num_vm_area = 1;
+    }
     while(ptr != NULL && ptr->vm_end <= addr){
         ptr = ptr->vm_next;
     }
@@ -316,8 +399,39 @@ long vm_area_pagefault(struct exec_context *current, u64 addr, int error_code)
     if(error_code == 0x6 && ((ptr->access_flags & O_WRITE) == 0)){
         return -1; // Write on area with no write access
     }
-    
-    return -1;
+    if(error_code == 0x7){
+        if((ptr->access_flags & O_WRITE) == 0) return -1;
+        /*VERY IMPORTANT - REPLACE WITH handle_cow_fault*/ return 1;
+    }
+    const long pru_flags = ((1ull << 4) | (1ull << 3) | 1ull);
+    long * pgd = osmap(current->pgd);
+    long pgd_off = ((addr & ((1ull << 48) - (1ull << 39))) >> 39);
+    if(!present_page(pgd[pgd_off])){
+        long pfnum = os_pfn_alloc(OS_PT_REG);
+        pgd[pgd_off] = ((pfnum << 12) | pru_flags);
+    }   
+    long * pud = osmap(pgd[pgd_off] >> 12);
+    long pud_off = ((addr & ((1ull << 39) - (1ull << 30))) >> 30);
+    if(!present_page(pud[pud_off])){
+        long pfnum = os_pfn_alloc(OS_PT_REG);
+        pud[pud_off] = ((pfnum << 12) | pru_flags);
+    }
+    long *pmd = osmap(pud[pud_off] >> 12);
+    long pmd_off = ((addr & ((1ull << 30) - (1ull << 21))) >> 21);
+    if(!present_page(pmd[pmd_off])){
+        long pfnum = os_pfn_alloc(OS_PT_REG);
+        pmd[pmd_off] = ((pfnum << 12) | pru_flags);
+    }
+    long * pte = osmap(pmd[pmd_off] >> 12);
+    long pte_off = ((addr & ((1ull << 21) - (1ull << 12))) >> 12);
+    if(!present_page(pte[pte_off])){
+        long pfnum = os_pfn_alloc(USER_REG);
+        int rw_flag = 0;
+        if((ptr->access_flags&O_WRITE) != 0) rw_flag = 1;
+        pte[pte_off] = ((pfnum << 12) | (1ull << 4) | (rw_flag << 3) | 1);
+    }
+
+    return 1;
 }
 
 /**
